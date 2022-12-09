782b5f61affd22e9750a0cc592d6bc5a70ea3c43
Washington Alejandro Pilligua Chiliguano <107654941+AlejandroPilligua@users.noreply.github.com>
2022-12-08 20:40:22 -0500

    Archives V2

---
+++ 06-Code/02 REST Full Web Services/TECHSTORE_API_REST/src/models/Users.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+const uniqueValidator = require("mongoose-unique-validator");
+
+const usersSchema = new Schema(
+  {
+    name_user: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+  },
+);
+
+usersSchema.plugin(uniqueValidator);
+export default model("Users", usersSchema);
