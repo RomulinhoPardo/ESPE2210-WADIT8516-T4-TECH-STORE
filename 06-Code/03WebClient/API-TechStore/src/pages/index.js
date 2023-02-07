4d802e1178fea494664ec25afdfeed4ec0b772ee
Romulo Agustin Pardo Salazar <rapardo1@espe.edu.ec>
2023-02-07 13:45:09 -0500

    DashboarPage.jx

---
+++ 06-Code/03WebClient/API-TechStore/src/pages/index.js
@@ -0,0 +1,10 @@
+export *from './ClientsPage'
+export * from './DashboardPage'
+export * from './HomePage'
+export * from './LoginPage'
+export * from './ProductMenu'
+export * from './ProductsPage'
+export * from './RegisterPage'
+export * from './UserMenu'
+export * from './UserPage'
+
