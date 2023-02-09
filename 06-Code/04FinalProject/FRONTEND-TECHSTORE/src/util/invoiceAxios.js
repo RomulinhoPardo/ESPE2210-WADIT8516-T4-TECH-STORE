5105743c532009a790d17c9ee5db4d127e04974f
Washington Alejandro Pilligua Chiliguano <wapilligua@espe.edu.ec>
2023-02-08 23:17:15 -0500

    Project final V1

---
+++ 06-Code/04FinalProject/FRONTEND-TECHSTORE/src/util/invoiceAxios.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3004/api/invoices";
+
+export async function GetInvoices() {
+  try {
+    const response = await axios.get(`${baseUrl}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function postInvoice(body) {
+  try {
+    const response = await axios.post(`${baseUrl}`, body);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function putInvoice(id, body) {
+  try {
+    const response = await axios.put(`${baseUrl}/${id}`, body);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteInvoices(id) {
+  try {
+    const response = await axios.delete(`${baseUrl}/${id}`);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ 06-Code/04FinalProject/FRONTEND-TECHSTORE/src/util/userAxios.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3004/api/users";
+
+export async function getUsers() {
+  try {
+    const users = await axios.get(`${baseUrl}`);
+    return users.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function postUser(body) {
+  try {
+    const user = await axios.post(`${baseUrl}`, body);
+    return user.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function putUser(id, body) {
+  try {
+    const user = await axios.put(`${baseUrl}/${id}`, body);
+    return user.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteUser(id) {
+  try {
+    const user = await axios.delete(`${baseUrl}/${id}`);
+    return user.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
